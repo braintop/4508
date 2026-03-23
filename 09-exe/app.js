aa8899bce704c57d3014b4f3538418b0b1aa7aa0
braintop <braintop7@gmail.com>
2026-03-23 21:26:04 +0200

    add

--- 09-exe/app.js
+++ 09-exe/app.js
@@ -4,6 +4,17 @@ function addItem(){
     let color = prompt("Enter color");
     items.push(item);
     document.getElementById("result").innerHTML = items + "<br>" + items.length;
-        document.body.style.backgroundColor =color;
+  document.body.style.backgroundColor =color;
     
+}
+
+function addItem2(){
+    let item = prompt("Enter item");
+    items.push(item);
+    let names ="";//david br shlomo br 
+    for(let i = 0; i < items.length; i++){
+        names = names + items[i] + "<br>";
+    }
+    console.log(names);
+    document.getElementById("result").innerHTML = names;
 }
