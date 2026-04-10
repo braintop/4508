7336a84000c86254fa2634b10add19b0b12e1406
braintop <braintop7@gmail.com>
2026-04-10 03:17:16 +0300

    Add

--- 15-dom/10-create-element/app.js
+++ 15-dom/10-create-element/app.js
@@ -2,8 +2,22 @@ function createCard(){
     const newDiv = document.createElement("div");
     newDiv.textContent = "I am new!";
     newDiv.classList.add("card");
-    
+    //console.log(newDiv);
     // Add to the page
     const container = document.querySelector("#container");
     container.appendChild(newDiv);
+
+    // container.append(
+    //     document.createElement("hr"),
+    //     "Some text"
+    //   );
+      
+}
+function removeCard1(){
+    const container = document.querySelector("#container");
+    container.removeChild(container.lastChild);
+}
+function removeCard2(){
+    const card = document.querySelector(".card ");
+    card.remove();
 }
