a393d644c2dd5379899b8233514ca4ac39bc4c91
braintop <braintop7@gmail.com>
2026-08-17 21:13:46 +0300

    Add

---
+++ 0/my_neoo_db/dist/app.js
@@ -0,0 +1,72 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const serverless_1 = require("@neondatabase/serverless");
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not set');
+}
+const sql = (0, serverless_1.neon)(databaseUrl);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/', (_req, res) => {
+    res.send('Hello Node');
+});
+app.get('/cities', async (_req, res) => {
+    try {
+        const cities = await sql `SELECT * FROM cities`;
+        res.json(cities);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to get cities' });
+    }
+});
+app.get('/cities/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const city = await sql `SELECT * FROM cities WHERE city_id = ${id}`;
+        res.json(city);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to get city' });
+    }
+});
+app.post('/cities', async (req, res) => {
+    try {
+        const { city_name } = req.body;
+        const city = await sql `INSERT INTO cities (city_name) VALUES (${city_name})`;
+        res.json(city);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to create city' });
+    }
+});
+app.put('/cities/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { city_name } = req.body;
+        const city = await sql `UPDATE cities SET city_name = ${city_name} WHERE city_id = ${id}`;
+        res.json(city);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to update city' });
+    }
+});
+app.delete('/cities/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const city = await sql `DELETE FROM cities WHERE city_id = ${id}`;
+        res.json(city);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to delete city' });
+    }
+});
+const server = app.listen(3002, () => {
+    console.log(`http://localhost:${3002}`);
+});
