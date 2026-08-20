aafbb3658452137ff401f1a44f52d514432db7c9
braintop <braintop7@gmail.com>
2026-08-20 18:15:15 +0300

    Add

--- 0/my_neoo_db/dist/app.js
+++ 0/my_neoo_db/dist/app.js
@@ -49,7 +49,7 @@ app.post('/cities', async (req, res) => {
 app.put('/cities/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const { city_name } = req.body;
+        const { city_name } = req.body; //{city_name: 'New York'}
         const city = await sql `UPDATE cities SET city_name = ${city_name} WHERE city_id = ${id}`;
         res.json(city);
     }
@@ -65,6 +65,36 @@ app.delete('/cities/:id', async (req, res) => {
     }
     catch (error) {
         res.status(500).json({ error: 'Failed to delete city' });
+    }
+});
+//======================COURSES======================
+app.get('/courses', async (req, res) => {
+    try {
+        const courses = await sql `SELECT * FROM courses`;
+        res.json(courses);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to get courses' });
+    }
+});
+app.get('/courses/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const course = await sql `SELECT * FROM courses WHERE course_id = ${id}`;
+        res.json(course);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to get course' });
+    }
+});
+app.post('/courses', async (req, res) => {
+    try {
+        const { course_name } = req.body;
+        const course = await sql `INSERT INTO courses (course_name) VALUES (${course_name})`;
+        res.json(course);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to create course' });
     }
 });
 const server = app.listen(3002, () => {
--- 0/my_neoo_db/src/app.ts
+++ 0/my_neoo_db/src/app.ts
@@ -55,7 +55,7 @@ app.post('/cities', async (req, res) => {
 app.put('/cities/:id', async (req, res) => {
   try {
     const { id } = req.params
-    const { city_name } = req.body
+    const { city_name } = req.body//{city_name: 'New York'}
     const city = await sql`UPDATE cities SET city_name = ${city_name} WHERE city_id = ${id}`
     res.json(city)
   } catch (error) {
@@ -73,7 +73,57 @@ app.delete('/cities/:id', async (req, res) => {
     res.status(500).json({ error: 'Failed to delete city' })
   }
 })
+//======================COURSES======================
 
+app.get('/courses', async (req, res) => {
+  try {
+    const courses = await sql`SELECT * FROM courses`
+    res.json(courses)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get courses' })
+  }
+})
+app.get('/courses/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const course = await sql`SELECT * FROM courses WHERE course_id = ${id}`
+    res.json(course)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get course' })
+  }
+})
+
+
+app.post('/courses', async (req, res) => {
+  try {
+    const { course_name } = req.body
+    const course = await sql`INSERT INTO courses (course_name) VALUES (${course_name})`
+    res.json(course)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create course' })
+  }
+})
+
+app.delete('/courses/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const course = await sql`DELETE FROM courses WHERE course_id = ${id}`
+    res.json(course)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete course' })
+  }
+})
+
+app.put('/courses/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const { course_name } = req.body
+    const course = await sql`UPDATE courses SET course_name = ${course_name} WHERE course_id = ${id}`
+    res.json(course)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update course' })
+  }
+})
 const server = app.listen(3002, () => {
   console.log(`http://localhost:${3002}`)
 })
